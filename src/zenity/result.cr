309be153c0f2b7a09cb4f6dbf4afdae246ad5a48
kojix2 <2xijok@gmail.com>
2024-04-07 11:29:09 +0900

    Add comments for documentation

--- src/zenity/result.cr
+++ src/zenity/result.cr
@@ -1,4 +1,7 @@
 class Zenity
+  # Represents the result of a zenity command execution.
+  # it delegates all missing methods to the Process::Status object
+  # See: https://crystal-lang.org/api/Process/Status.html
   class Result
     getter command : String
     getter output : String?
--- src/zenity/version.cr
+++ src/zenity/version.cr
@@ -1,3 +1,4 @@
 class Zenity
+  # Defined in the `shard.yml` file
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 end
